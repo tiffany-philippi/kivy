ace9c50be69df75a8f3ec50d015ecabef7f6d4c0
Tiffany Philippi <47127551+tiffany-philippi@users.noreply.github.com>
2022-04-08 11:59:35 -0300

    adding a function to the button

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ from kivy.uix.boxlayout import BoxLayout
 class Screen(App): 
     def build(self):
         box = BoxLayout(orientation='vertical')
-        button = Button(text="Hello world")
-        label = Label(text="This is a label")
+        button = Button(text="Hello world", font_size=30, on_release=self.increment)
+        label = Label(text="This is a label", font_size=21)
         box.add_widget(button)
         box.add_widget(label)
+        
         return box
+
+    def increment(self, button):
+        button.text = 'You\'re welcome'
 
 Screen().run()
