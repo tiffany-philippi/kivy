659dd0dc22aa7e44e1b64a5dc75b6a7781bea2a9
Tiffany Philippi <47127551+tiffany-philippi@users.noreply.github.com>
2022-04-08 15:59:39 -0300

    Creating a new kv file

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 from kivy.app import App
-from kivy.uix.button import Button 
-from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout 
+
+class Increment(BoxLayout):
+    pass
 
 class Screen(App): 
     def build(self):
-        box = BoxLayout(orientation='vertical')
-        button = Button(text="How many times did I click?", font_size=30, on_release=self.increment)
-        self.label = Label(text="0", font_size=21)
-        box.add_widget(button)
-        box.add_widget(self.label)
-        
-        return box
-
-    def increment(self, button):
-        self.label.text = str(int(self.label.text)+1)
+        return Increment()
 
 Screen().run()
