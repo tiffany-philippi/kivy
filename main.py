8931a28135c0ea677d529a8be198ff1a18356d4d
Tiffany Philippi <47127551+tiffany-philippi@users.noreply.github.com>
2022-04-08 16:40:58 -0300

    creating a to do list with a scroll component

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout 
+from kivy.uix.scrollview import ScrollView 
+from kivy.uix.label import Label
 
-class Increment(BoxLayout):
-    pass
+class TasksList(ScrollView):
+    def __init__(self, tasks, **kwargs):
+        super().__init__(**kwargs)
+        for task in tasks:
+            self.ids.box.add_widget(Label(text=task, font_size = 21, size_hint_y = None, height = 200))
 
 class Screen(App): 
     def build(self):
-        return Increment()
+        return TasksList(['Go shopping', 'Pick up the kids', 'Walk the dog', 'Study for exames', 'Read a book'])
 
 Screen().run()
