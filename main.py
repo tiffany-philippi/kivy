e596ec5ffa4305d9608e83e3c6fcf86be93b2b72
Tiffany Philippi <47127551+tiffany-philippi@users.noreply.github.com>
2022-04-08 17:06:03 -0300

    Adding a new task

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from kivy.app import App
-from kivy.uix.scrollview import ScrollView 
 from kivy.uix.boxlayout import BoxLayout 
-from kivy.uix.label import Label
 
-class TasksList(ScrollView):
+class TasksList(BoxLayout):
     def __init__(self, tasks, **kwargs):
         super().__init__(**kwargs)
         for task in tasks:
             self.ids.box.add_widget(Task(text=task))
+
+    def addWidget(self):
+        text = self.ids.task_input.text
+        self.ids.box.add_widget(Task(text = text))
+        self.ids.task_input.text = ''
 
 class Task(BoxLayout):
     def __init__(self, text='', **kwargs):
