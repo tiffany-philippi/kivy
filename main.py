249e32e214d1e4266adae34ead8170f1b92e9bab
Tiffany Philippi <47127551+tiffany-philippi@users.noreply.github.com>
2022-04-08 16:56:39 -0300

    removing an item from list

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from kivy.app import App
 from kivy.uix.scrollview import ScrollView 
+from kivy.uix.boxlayout import BoxLayout 
 from kivy.uix.label import Label
 
 class TasksList(ScrollView):
     def __init__(self, tasks, **kwargs):
         super().__init__(**kwargs)
         for task in tasks:
-            self.ids.box.add_widget(Label(text=task, font_size = 21, size_hint_y = None, height = 200))
+            self.ids.box.add_widget(Task(text=task))
+
+class Task(BoxLayout):
+    def __init__(self, text='', **kwargs):
+        super().__init__(**kwargs)
+        self.ids.label_task.text = text
 
 class Screen(App): 
     def build(self):
