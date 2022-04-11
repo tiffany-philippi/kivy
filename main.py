6ba186fe2c2593c06484ff169bad60fb59668caa
Tiffany Philippi <47127551+tiffany-philippi@users.noreply.github.com>
2022-04-11 11:36:19 -0300

    creating a ActionBar with back and logout buttons

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout 
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 
 class Manager(ScreenManager):
+    pass
+
+class Menu(Screen):
     pass
 
 class TasksList(Screen):
@@ -10,6 +14,17 @@ class TasksList(Screen):
         super().__init__(**kwargs)
         for task in tasks:
             self.ids.box.add_widget(Task(text=task))
+
+    def on_pre_enter(self):
+        Window.bind(on_keyboard=self.back)
+
+    def back(self, window, key, *args):
+        if key == 27:
+            App.get_running_app().root.current = 'menu'
+            return True
+
+    def on_pre_leave(self):
+        Window.unbind(on_keyboard=self.back)
 
     def addWidget(self):
         text = self.ids.task_input.text
